dbdb4dbaa89f58b0011aed73699694685a88b63e
filipdavidovic <filip.davidovic.fd@gmail.com>
2017-03-14 14:44:03 +0100

    list.js fixed

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,28 @@
+// list.js
 var options = {
-    valueNames: ['name', 'description']
+    valueNames: ['name', 'description', 'faculty']
 };
 
 var facultiesList = new List('faculties-list', options);
+
+var loadListJs = function () {
+    var options = {
+        valueNames: ['name', 'description', 'faculty']
+    };
+
+    var facultiesList = new List('faculties-list', options);
+};
+
+var MyLibrary = {};
+
+MyLibrary.hash = "#bachelorRow";
+
+$('a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
+    var target = $(e.target).attr("href"); // activated tab
+    var tabPane = $(target);
+    var hash = MyLibrary.hash;
+    $(hash).removeClass('list'); // remove the list class from the  previous .tab-pane > .row
+    var rowDiv = $(tabPane).find('.row').addClass('list'); // find the activated tab-pane row and add the list class to it
+    MyLibrary.hash = '#' + rowDiv.attr('id'); // hash the current row id
+    loadListJs();
+});
