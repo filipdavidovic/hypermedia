08341be675b7949dc783a00a556f3f3c76d12ada
filipdavidovic <filip.davidovic.fd@gmail.com>
2017-03-16 19:29:24 +0100

    updated db

--- app_api/models/faculties.js
+++ app_api/models/faculties.js
@@ -1,44 +1,32 @@
 var mongoose = require('mongoose');
 
-var subjectSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    quartile: {type: Number, required: true}
-});
-
-var yearSchema = new mongoose.Schema({
-    yearNumber: {type: Number, required: true},
-    yearSubjects: [subjectSchema]
-});
-
 var bachelorSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
     name: {type: String, required: true},
     description: {type: String, required: true},
     banner: {type: String, required: true}
 });
 
 var premasterSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
     name: {type: String, required: true},
     description: {type: String, required: true},
     banner: {type: String, required: true}
 });
 
 var masterSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
     name: {type: String, required: true},
     description: {type: String, required: true},
     banner: {type: String, required: true}
 });
 
 var facultiesSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
     name: {type: String, required: true},
-    identification: {type: String, required: true},
     bachelor: [bachelorSchema],
     premaster: [premasterSchema],
     master: [masterSchema]
-    // , https://static.tue.nl/fileadmin/_processed_/3/f/csm_meisjemetrobot_01_1718ebf138.jpg - Happy Robot/CS
-    // subjects: [yearSchema]
 });
 
 mongoose.model('Faculty', facultiesSchema, 'faculties');
-
-
-// .findOne({identification: ...})
